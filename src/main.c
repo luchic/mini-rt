6204f79445cef72e7911fd4edc04d9c20ca3ad74
Nikita Luchinin <nluchini@student.42heilbronn.de>
2025-11-11 11:21:00 +0100

    Add main

---
+++ src/main.c
@@ -0,0 +1,25 @@
+
+#include "ft_minirt.h"
+#include <string.h>
+
+
+
+int	main(int argc, char **argv)
+{
+	t_app app;
+
+	if (argc != 2)
+		ft_exit("Usage: ./miniRT <scene.rt>", 1);
+	init_app(&app, argv[1]);
+	render(&app);
+
+	if (!app.tetris_enabled && find_first(&app.scene, OBJ_SPHERE))
+		shfx_trigger(&app);
+
+	setup_hooks(&app);
+	mlx_loop(app.mlx);
+	image_destroy(&app);
+	if (app.mlx)
+		mlx_terminate(app.mlx);
+	return (0);
+}
